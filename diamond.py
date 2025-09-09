fe3c67873857e3a2ef4b893d9ecff2354543260d
joshuagalisteo16-png <joshuagalisteo16@gmail.com>
2025-09-09 11:24:29 -0700

    program star
    My program creates a star

--- diamond.py
+++ diamond.py
@@ -3,5 +3,16 @@ This was created with my partner
 September 8th, 2025
 """
 
+# Required variables
+counter,rowsdDrawn,spacesOutside,spaceInside,starstoDraw,userInput = 0,0,0,0,0,0
+# string variables required 
+output = ""
+num1 = int(input("Enter your first odd number (1-13) Please: "))
 
-num1 = int(input("Enter your first odd number (1-13) Please "))
+# this is gonna do my top part
+#Im using a method/function to organize
+def top_diamond(num1): 
+    topString = "*"
+    return topString
+output = top_diamond(num1)
+print(output)

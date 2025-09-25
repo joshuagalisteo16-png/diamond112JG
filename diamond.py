a8ea64ae14a160a0b5fb70fe8699baa415cfd366
joshuagalisteo16-png <joshuagalisteo16@gmail.com>
2025-09-25 11:21:21 -0700

    Completed diamond
    adding onto diamond so that it will be finished

--- diamond.py
+++ diamond.py
@@ -7,53 +7,29 @@ September 8th, 2025
 counter, rowsDrawn, spacesOutside, spacesInside, starstoDraw, userInput = 0,0,0,0,0,0
 #
 #I'm using a function to organize.
-userInput=int(input("Enter an odd number (1-13): "))
+userInput = int(input("Enter an odd number (1-13): "))
 
-<<<<<<< HEAD
 def topDiamond(userInput):
     rowsDrawn= 0
     spacesOutside = userInput // 2 #starts first star at middle row. Num is rounded up.
     spacesInside= -1 #starts at 1 to ignore the "space" at the first star.
-=======
-def top_diamond(starstoDraw): 
-    spacesOutside = userInput // 2
-    #Start number of spaces between stars - starts at -1 for the top point
-    spacesInside = -1 
-    #Loop to draw each line of the top half of the diamond
-    rowsDrawn = userInput // 2 + 1
-    for i in range(rowsDrawn):
-        #Print spaces on the outside (left side)
-        print(" " * spacesOutside, end="")
-        print("*", end="")
-        #If not the first line, print inside spaces and the second star 
-        #Decrease the outside and increase the spaces inside for next row
-         if spacesInside >= 0:
-            print(" " * spacesInside, end="")
-            print("*", end="")
-        #Move to the next line
-        print()
-        #Decrease outside spaces and increase inside spaces for next row
-        spacesOutside -= 1
-        spacesInside += 2
->>>>>>> db380acbf0917bf623f9308b73434c945d965df5
-
     while rowsDrawn <= userInput // 2:
-        output= " " #begins a new row.
+        starstoDraw = " " #begins a new row.
         #down below draws the spaces outside the first star.
         counter = 0
         while counter < spacesOutside:
-            output += " "
+            starstoDraw += " "
             counter += 1
-        output += "*"
+        starstoDraw += "*"
         #adds a space outside and a star at the end of the output
         #below draws spaces inside beginning from the 2nd star
         if spacesInside > 0:
             counter = 0
             while counter < spacesInside:
-                output += " "
+                starstoDraw += " "
                 counter += 1
-            output += "*"
-        print(output)
+            starstoDraw += "*"
+        print(starstoDraw)
         rowsDrawn += 1 #each row is added until the user input number
         spacesOutside -= 1 #the space outside is subtracted by 1 each row
         spacesInside += 2 #each space inside is added by 2 each row. Ex: -1+2 =1. 1 space inside.
@@ -64,25 +40,38 @@ def bottomDiamond(userInput):
     spacesInside= userInput - 4 #begin with num 2 fewer than userinput to be adjacent to 2nd to last num.
     
     while rowsDrawn < userInput // 2:
-        output= " "
+        starstoDraw= " "
         counter = 0
         while counter < spacesOutside:
-            output += " "
+            starstoDraw += " "
             counter += 1
-        output += "*"
+        starstoDraw += "*"
         
         if spacesInside > 0:
             counter = 0
             while counter < spacesInside:
-                output += " "
+                starstoDraw += " "
                 counter += 1
-            output += "*"
-        print(output)
-        
+            starstoDraw += "*"
+        print(starstoDraw)
         rowsDrawn += 1
         spacesOutside += 1
         spacesInside -= 2
-        return userInput
 
+def get_valid_input():
+    while True:
+        try:
+            userInput = int(input("Enter an odd number from 1 to 13: "))
+            if userInput < 1 or userInput > 13:
+                print("Number must be between 1 and 13.")
+            elif userInput % 2 == 0:
+                print("Number must be odd.")
+            else:
+                return userInput
+        except ValueError:
+            print("Please enter a valid integer.")
+    return userInput
+userInput = get_valid_input()
+print(userInput)
 topDiamond(userInput)
 bottomDiamond(userInput)
